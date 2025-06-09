e47adc8d8862018562b3ceac96f60bc5675b5a04
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-06-10 00:17:14 +0200

    Increase version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.solaris_android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
