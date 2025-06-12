447d8130aeb0852edcfeb75d23c7a7db7553e087
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-06-13 00:45:57 +0200

    Add app deep link support

--- app/src/main/java/com/example/solaris_android/MainActivity.java
+++ app/src/main/java/com/example/solaris_android/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.solaris_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Window;
@@ -28,6 +29,19 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
 
         webView.setWebViewClient(new MyBrowser());
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            var data = intent.getData();
+
+            if (data != null && data.getHost() != null) {
+                if (data.getHost().equals(getResources().getString(R.string.domain))) {
+                    webView.loadUrl(data.toString());
+                    return;
+                }
+            }
+        }
+
         webView.loadUrl("https://solaris.games");
     }
 
