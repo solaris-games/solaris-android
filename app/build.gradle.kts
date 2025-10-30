1eab2a618ffa3fe6fde801572b613a49c419e12f
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2025-10-30 19:16:59 +0100

    Increment version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.voxel.solaris_android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.2"
+        versionCode = 6
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
